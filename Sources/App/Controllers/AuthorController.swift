5a3bb860b8c7cd3f4159d1d3cffc5033beb1aebe
iMac 21inch <davej@imac-21inch.local>
2019-05-09 16:27:10 -0500

    updated comments

--- Sources/App/Controllers/AuthorController.swift
+++ Sources/App/Controllers/AuthorController.swift
@@ -7,8 +7,13 @@
 import Foundation
 import Vapor
 
+/// AuthorController is the controller of the Author ORM via HTTP verbs.
 final class AuthorController: RouteCollection {
     
+    /// Define the routes for the Author ORM.
+    ///
+    /// - Parameter router: the application router
+    /// - Throws: any error when defining the routes
     func boot(router: Router) throws {
         let authors = router.grouped("authors")
     
@@ -22,18 +27,42 @@ final class AuthorController: RouteCollection {
         
     }
     
+    /// Get list all authors: /authors.
+    ///
+    /// - Parameter request: the request
+    /// - Returns: array of Author objects
+    /// - Throws: any error
     func list(_ request: Request) throws -> Future<[Author]> {
         return Author.query(on: request).all()
     }
     
+    /// Get a specific author: /authors/1, etc.
+    ///
+    /// - Parameter request: the request
+    /// - Returns: the specific author
+    /// - Throws: any error
     func show(_ request: Request) throws -> Future<Author> {
         return try request.parameters.next(Author.self)
     }
     
+    /// Post an author to the ORM.
+    ///
+    /// - Parameters:
+    ///   - request: the request
+    ///   - author: the request body author
+    /// - Returns: the author from the ORM
+    /// - Throws: any error
     func create(_ request: Request, author:Author) throws -> Future<Author> {
         return author.save(on: request)
     }
     
+    /// Put an author to the ORM.
+    ///
+    /// - Parameters:
+    ///   - request: the request
+    ///   - body: the request body author to update
+    /// - Returns: the updated author from the ORM
+    /// - Throws: any error
     func update(_ request: Request, _ body: AuthorContent)throws -> Future<Author> {
         let author = try request.parameters.next(Author.self)
         return author.map(to: Author.self, { author in
@@ -43,10 +72,20 @@ final class AuthorController: RouteCollection {
         }).update(on: request)
     }
     
+    /// Delete an author from the ORM.
+    ///
+    /// - Parameter request: the request with the author id as the parameter
+    /// - Returns: HTTP status 200
+    /// - Throws: any error
     func delete(_ request: Request)throws -> Future<HTTPStatus> {
         return try request.parameters.next(Author.self).delete(on: request).transform(to: .noContent)
     }
     
+    /// Get a list of Work objects related to this author.
+    ///
+    /// - Parameter req: the request
+    /// - Returns: a list of work objects from the ORM
+    /// - Throws: any error
     func getWorks(_ req: Request) throws -> Future<[Work]> {
         return try req.parameters.next(Author.self).flatMap(to: [Work].self) { (author) in
             return try author.works.query(on: req).all()
@@ -55,11 +94,14 @@ final class AuthorController: RouteCollection {
     
 }
 
+/// AuthorContent model (used for the PUT).
 struct AuthorContent:Content {
     var firstName: String?
     var lastName: String?
 }
 
+
+/// The author and works view model.
 struct AuthorView: Encodable {
     var author: Author
     var works: Future<[Work]>
--- Sources/App/Controllers/WorkController.swift
+++ Sources/App/Controllers/WorkController.swift
@@ -8,8 +8,13 @@
 import Foundation
 import Vapor
 
+/// WorkController is the controller of the Work ORM via HTTP verbs.
 final class WorkController: RouteCollection {
     
+    /// Define the routes for the Work ORM.
+    ///
+    /// - Parameter router: the application router
+    /// - Throws: any error when defining the routes
     func boot(router: Router) throws {
         let works = router.grouped("works")
         
@@ -23,18 +28,42 @@ final class WorkController: RouteCollection {
 
     }
     
+    /// Get list all works: /works.
+    ///
+    /// - Parameter request: the request
+    /// - Returns: array of Work objects
+    /// - Throws: any error
     func list(_ request: Request) throws -> Future<[Work]> {
         return Work.query(on: request).all()
     }
     
+    /// Get a specific work: /works/1, etc.
+    ///
+    /// - Parameter request: the request
+    /// - Returns: the specific work
+    /// - Throws: any error
     func show(_ request: Request) throws -> Future<Work> {
         return try request.parameters.next(Work.self)
     }
     
+    /// Post work to the ORM.
+    ///
+    /// - Parameters:
+    ///   - request: the request
+    ///   - author: the request body work
+    /// - Returns: the work from the ORM
+    /// - Throws: any error
     func create(_ request: Request, work:Work) throws -> Future<Work> {
         return work.save(on: request)
     }
     
+    /// Put an work to the ORM.
+    ///
+    /// - Parameters:
+    ///   - request: the request
+    ///   - body: the request body work to update
+    /// - Returns: the updated work from the ORM
+    /// - Throws: any error
     func update(_ req: Request) throws -> Future<Work> {
         return try flatMap(to: Work.self, req.parameters.next(Work.self), req.content.decode(Work.self)) { (work, updatedWork) in
             work.title = updatedWork.title
@@ -43,10 +72,20 @@ final class WorkController: RouteCollection {
         }
     }
     
+    /// Delete a work from the ORM.
+    ///
+    /// - Parameter request: the request with the work id as the parameter
+    /// - Returns: HTTP status 200
+    /// - Throws: any error
     func delete(_ request: Request)throws -> Future<HTTPStatus> {
         return try request.parameters.next(Work.self).delete(on: request).transform(to: .noContent)
     }
     
+    /// Get the author from the given work: /works/1/author.
+    ///
+    /// - Parameter req: the request
+    /// - Returns: the author from the ORM
+    /// - Throws: any error
     func getAuthor(_ req: Request) throws -> Future<Author> {
         return try req.parameters.next(Work.self).flatMap(to: Author.self) { (work) in
             return work.author.get(on: req)
